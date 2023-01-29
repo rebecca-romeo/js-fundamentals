1bb8d221c19750ff93edea59aa44dc352d11bbb6
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-01-29 16:49:17 -0500

    created while loop to list packing items

--- packing-list.js
+++ packing-list.js
@@ -1,7 +1,16 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+// using a  for loop to list each item
 console.log("Kitchen stuff to pack:");
-
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+
+// using a while loop to list each item
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
